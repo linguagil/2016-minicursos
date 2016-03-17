f6af2a29de2cdcaafb60be84630f6b9d107e96eb
Mauricio Vieira <mauricio@mauriciovieira.net>
2016-03-17 11:04:49 -0300

    Remove unused django apps and middlewares. Add django rest framework and django_extensions

--- project/scrum/settings.py
+++ project/scrum/settings.py
@@ -31,22 +31,23 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions',
+
+    # rest framework
+    'rest_framework',
+    'rest_framework.authtoken',
+
+    # app
+    'board',
 ]
 
 MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
